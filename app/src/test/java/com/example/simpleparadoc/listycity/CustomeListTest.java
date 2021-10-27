8091ec4d9756221f4283756e17913166467b5d41
Cooperthedog2 <70604965+Cooperthedog2@users.noreply.github.com>
2021-10-27 17:32:50 -0600

    Green

---
+++ app/src/test/java/com/example/simpleparadoc/listycity/CustomeListTest.java
@@ -0,0 +1,29 @@
+package com.example.simpleparadoc.listycity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CustomeListTest {
+
+    private CustomList list;
+
+    @BeforeEach
+    public void  createlist(){
+        list=new CustomList(null, new ArrayList<City>());
+    }
+    @Test
+    public void addCitytest(){
+        int listSize=list.getCount();
+        list.addCity(new City("Edmonton","Alberta"));
+        assertEquals(listSize+1,list.getCount());
+    }
+
+
+}
